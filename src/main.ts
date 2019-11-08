e7f5ff3c7985831a9288101c69733732db46f0ef
unknown <etienne.duverney@viacsi.fr>
2019-11-08 18:50:19 +0100

    Index page

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  console.log(join(__dirname, '\\static'))
+  app.useStaticAssets(join(__dirname, '\\static'))
+  await app.listen(3000, () => {
+    console.log(`Server listening at http://localhost:3000/`)
+  })
 }
 bootstrap();
